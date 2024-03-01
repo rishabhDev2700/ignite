f35c8b82af8bd101cb20f35489ac103ec74cce70
rishabhDev2700 <rishavkumar2700@gmail.com>
2024-03-01 20:01:13 +0530

    Updated menu toggle and about page

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-
-$("#menu").click(() => {
+function menuToggle() {
     $("#menu-button").toggle("fast")
     $("#menu-button").toggleClass("hidden")
     $("#close-button").toggle("fast")
     $("#close-button").toggleClass("hidden")
     $("nav").toggleClass("-translate-y-80 shadow-gray-300")
+}
 
+$("#menu").click(() => {
+    menuToggle()
+})
+$(".nav-link").click(() => {
+    menuToggle()
 })
